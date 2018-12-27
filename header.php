0cfa656a6d8f96156008a6cd54365095968a987e
tom <tomdeng.iphone@gmail.com>
2018-12-27 14:44:49 -0800

    First commit after static html conversion

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+  <title>Welcome to ML Building Supply!</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="author" content="Tom Deng" />
+  <meta name="description" content="ML Building Supply LTD">
+  <meta name="keywords" content="Aluminum,Bending, Stainless, product, building, supply">
+  <script type="text/javascript" src="jquery.min.js"></script>
+</head>
+<html>
+<body>
+  <div id="wrapper">
+    <div id="header">
+      <h1>ML Building Supply Ltd.</h1>
+      <div id="header_right">
+          <ul>
+            <li id="phone">+1 604-430-3008</li>
+            <li id="email"><a href="mailto:mlbuildingsupplysales@gmail.com">mlbuildingsupplysales@gmail.com</a></li>
+          </ul>
+      </div>
+    </div> <!-- div header -->
